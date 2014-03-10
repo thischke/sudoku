5227012476264f93249c8a12ff8b648a728c6824
thischke <thorben.hischke@googlemail.com>
2014-03-10 20:57:00 +0100

    added templates, first draft of commandButton 'resolve'

--- src/main/java/de/thischke/sudoku/beans/BoardBean.java
+++ src/main/java/de/thischke/sudoku/beans/BoardBean.java
@@ -1,10 +1,12 @@
 package de.thischke.sudoku.beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -16,9 +18,11 @@ import de.thischke.sudoku.strategy.ClearPossibilites;
 import de.thischke.sudoku.strategy.Strategy;
 
 @SuppressWarnings("restriction")
-@ManagedBean(name="board")
+@ManagedBean(name = "board")
 @SessionScoped
-public class BoardBean {
+public class BoardBean implements Serializable {
+
+	private static final long serialVersionUID = 134569483576947560L;
 
 	private String title;
 	private Board board;
@@ -28,7 +32,7 @@ public class BoardBean {
 	public void init() {
 		this.board = new Board();
 		this.title = "Sudoku";
-		
+
 		fields = new ArrayList<ArrayList<Field>>();
 		for (int y = 0; y < 9; y++) {
 			ArrayList<Field> columns = new ArrayList<Field>();
@@ -46,16 +50,21 @@ public class BoardBean {
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	public void updateField(ActionEvent actionEvent) {
-		Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-		
+		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+
 		Integer x = Integer.valueOf(params.get("x"));
 		Integer y = Integer.valueOf(params.get("y"));
 		Integer wert = Integer.valueOf(params.get("wert"));
 		board.setFieldValue(x, y, wert);
-		
+
 		Strategy strategy = new ClearPossibilites();
 		strategy.resolver(board);
+	}
+
+	public void resolve(ActionEvent actionEvent) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage("", "resolve board"));
 	}
 }
